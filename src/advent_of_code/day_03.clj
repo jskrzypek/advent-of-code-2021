5fefeec73623da01eb3076db27e436251883941e
Joshua Skrzypek <jskrzypek@gmail.com>
2021-12-13 00:27:59 -0500

    feat: Day 3 pt 1
    Having trouble with pt 2...

--- src/advent_of_code/day_03.clj
+++ src/advent_of_code/day_03.clj
@@ -1,11 +1,83 @@
-(ns advent-of-code.day-03)
+(ns advent-of-code.day-03
+  (:require [aocd.core :as aoc]
+            [cuerdas.core :as str]
+            [clojure.core.matrix :as mat]))
+
+(defn parse
+  [s]
+  (-> s
+      (str/split)
+      (->>
+       (mapv (comp
+              (partial mapv read-string)
+              #(str/split %1 ""))))))
+
+(def input
+  (parse (aoc/input 2021 3)))
+
+(def example
+  (parse "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"))
+
+(defn prp
+  [x]
+  (prn x)
+  x)
+
+(defn bitwise-reduce
+  [f mm]
+  (->> mm mat/transpose f mat/transpose))
+
+(defn bit-reducer
+  [v]
+  )
+
+(defn mcbs
+  [mm]
+  (bitwise-reduce 
+   (fn [m] (mapv #(if (<= (/ (count %) 2) (apply + %)) 1 0) m))
+   mm))
+
+(defn lcbs
+  [mm]
+  (bitwise-reduce
+   (fn [m] (mapv #(if (>= (/ (count %) 2) (apply + %)) 1 0) m))
+   mm))
+
+(defn γ*ε
+  [mm]
+  (->> mm
+       ((juxt mcbs lcbs))
+       (mapv (comp #(Integer/parseInt % 2) str/join))
+       (#(apply * %))))
+
+(defn O₂
+  [mm]
+  (->> mm
+       mcbs
+       (map-indexed (fn [n bit] (filter #(= (nth % n) bit) mm)))))
+
+(comment
+  (mcbs example)
+  (->> example mcbs (map-indexed vector))
+  (O₂ example)
+  (γ*ε example))
 
 (defn part-1
   "Day 03 Part 1"
   []
-  )
+  (γ*ε input))
 
 (defn part-2
   "Day 03 Part 2"
-  []
-  )
+  [])
